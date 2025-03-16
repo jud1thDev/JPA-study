5c17d61bdf6eee85e81d3d2a3bf8ac4f80d3c3c2
judith <8w8u8@naver.com>
2025-03-16 19:00:22 +0900

    docs: 2.5장까지

--- CH02-JPA시작/ch02-jpa-start1/src/main/java/jpabook/start/Member.java
+++ CH02-JPA시작/ch02-jpa-start1/src/main/java/jpabook/start/Member.java
@@ -1,23 +1,23 @@
 package jpabook.start;
 
-import javax.persistence.*;  //**
+import javax.persistence.*;  // JPA 어노테이션 패키지
 
 /**
  * User: HolyEyE
  * Date: 13. 5. 24. Time: 오후 7:43
  */
-@Entity
-@Table(name="MEMBER")
+@Entity // 이 클래스를 테이블과 매핑한다고 JPA에게 알려준다.
+@Table(name="MEMBER") // 엔티티 클래스에 매핑할 테이블 정보를 알려준다. name 속성을 활용했다.
 public class Member {
 
-    @Id
+    @Id // 식별자 필드 표시
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME") // 필드를 컬럼에 매핑한다.
     private String username;
 
-    private Integer age;
+    private Integer age; // @Column을 생략하면 필드명(여기선 age)이 컬럼명이 된다.
 
     public String getId() {
         return id;
